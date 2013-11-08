588b658532f79ee9e5a981c1aa66c8becc51c5d1
Babbitt huang <bhuang@bbtechgroup.com>
2013-11-08 20:12:56 +0800

    ToDoMVC-angularJS

---
+++ app/js/controllers/todo.js
@@ -0,0 +1,91 @@
+/*global define*/
+'use strict';
+
+/**
+ * The main controller for the app. The controller:
+ * - retrieves and persist the model via the todoStorage service
+ * - exposes the model to the template and provides event handlers
+ */
+
+define(['app', 'services/todoStorage'], function (app) {
+        console.log("aaaa");
+        return app.controller('TodoController', ['$scope', '$location', 'todoStorage', 'filterFilter',
+                function TodoController($scope, $location, todoStorage, filterFilter) {
+                        console.log('controller');
+                        var todos = $scope.todos = todoStorage.get();
+                        debugger
+                        $scope.newTodo = '';
+                        $scope.editedTodo = null;
+                        
+                        $scope.$watch('todos', function () {
+                                console.log('aaaa-wat');
+                                $scope.remainingCount = filterFilter(todos, { completed: false }).length;
+                                $scope.doneCount = todos.length - $scope.remainingCount;
+                                $scope.allChecked = !$scope.remainingCount;
+                                todoStorage.put(todos);
+                                
+                        }, true);
+
+                        if ($location.path() === '') {
+                                $location.path('/');
+                        }
+
+                        $scope.location = $location;
+
+                        $scope.$watch('location.path()', function (path) {
+                                $scope.statusFilter = (path === '/active') ?
+                                        { completed: false } : (path === '/completed') ?
+                                        { completed: true } : null;
+                        });
+
+
+                        $scope.addTodo = function () {
+                                var newTodo = $scope.newTodo.trim();
+                                if (!newTodo.length) {
+                                        return;
+                                }
+                                todos.push({
+                                        title: newTodo,
+                                        completed: false
+                                });
+
+                                $scope.newTodo = '';
+                        };
+
+
+                        $scope.editTodo = function (todo) {
+                                debugger
+                                $scope.editedTodo = todo;
+                        };
+
+
+                        $scope.doneEditing = function (todo) {
+                                $scope.editedTodo = null;
+                                todo.title = todo.title.trim();
+
+                                if (!todo.title) {
+                                        $scope.removeTodo(todo);
+                                }
+                        };
+
+
+                        $scope.removeTodo = function (todo) {
+                                todos.splice(todos.indexOf(todo), 1);
+                        };
+
+
+                        $scope.clearDoneTodos = function () {
+                                $scope.todos = todos = todos.filter(function (val) {
+                                        return !val.completed;
+                                });
+                        };
+
+
+                        $scope.markAll = function (done) {
+                                todos.forEach(function (todo) {
+                                        todo.completed = done;
+                                });
+                        };
+                }
+        ]);
+});
